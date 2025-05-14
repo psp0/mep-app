cb9a53e19a219ea1cdaeb989a073968501ec5322
psp0 <tkdvlf9058@g.hongik.ac.kr>
2025-05-14 21:04:01 +0900

    feat: Update Swagger configuration to use environment variables for server URL and port

--- src/main/java/com/example/mepapp/config/SwaggerConfig.java
+++ src/main/java/com/example/mepapp/config/SwaggerConfig.java
@@ -5,11 +5,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server.url:https://localhost}")
+    private String serverUrl;
+
+    @Value("${swagger.server.port:8080}")
+    private String serverPort;
 
     @Bean
     public OpenAPI openAPI() {
@@ -24,9 +32,14 @@ public class SwaggerConfig {
                 .in(SecurityScheme.In.HEADER).name("Authorization");
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
+        Server server = new Server()
+                .url(serverUrl + ":" + serverPort)
+                .description("MEP App Server");
+
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
                 .addSecurityItem(securityRequirement)
+                .addServersItem(server)
                 .info(info);
     }
 }
